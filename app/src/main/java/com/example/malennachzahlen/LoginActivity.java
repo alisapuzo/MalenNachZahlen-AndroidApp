d1505c4201ac126953f2ac1e9d933d8c119ef0fd
malinschtz <malin.schuetz136@gmail.com>
2026-02-05 14:12:08 +0100

    Überflüssige imports entfernt

--- app/src/main/java/com/example/malennachzahlen/LoginActivity.java
+++ app/src/main/java/com/example/malennachzahlen/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.example.malennachzahlen;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
@@ -9,11 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 
